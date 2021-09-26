885e62bb5974a57a90ae8f206e20acedbc638c21
jas.zheng <jas.zheng@vipshop.com>
2021-09-26 09:21:22 +0800

    完全去除json_output里的缓存

--- src/you_get/json_output.py
+++ src/you_get/json_output.py
@@ -49,12 +49,12 @@ def print_info(site_info=None, title=None, type=None, size=None):
     ve.url = None
 
 def download_urls(urls=None, title=None, ext=None, total_size=None, refer=None):
-    ve = last_info
-    if not ve:
-        ve = VideoExtractor()
-        ve.name = ''
-        ve.url = urls
-        ve.title=title
+    # ve = last_info
+    # if not ve:
+    ve = VideoExtractor()
+    ve.name = ''
+    ve.url = urls
+    ve.title=title
     # save download info in streams
     stream = {}
     stream['container'] = ext
